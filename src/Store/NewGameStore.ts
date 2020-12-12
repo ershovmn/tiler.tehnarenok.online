9723fd58f13fae76a778db5c3f203500a59edebe
tehnarenok <tehnarenok@gmail.com>
2020-12-12 09:53:36 +0300

    изменение сторов

--- src/Store/NewGameStore.ts
+++ src/Store/NewGameStore.ts
@@ -1,49 +1,74 @@
 import React, { useEffect, useState } from 'react'
 import { createContainer } from 'react-gstore'
 
-function saveToLocalStorage() {
-    return (target: Object, propertyName: string, descriptor: any) => {
-        const method = descriptor.value;
-        descriptor.value = function(...args : any[]) {
-            console.log(args)
-            let str = localStorage.getItem(propertyName)
-            let abc = str ? JSON.parse(str) : null
-            const result = method.apply(this, abc ? [abc] : args);
-            console.log(propertyName)
-            localStorage.setItem(propertyName, JSON.stringify(result)) 
-            return result;
-        };
-    };
+// function saveToLocalStorage() {
+//     return (target: Object, propertyName: string, descriptor: any) => {
+//         const method = descriptor.value;
+//         descriptor.value = function(...args : any[]) {
+//             console.log(args)
+//             let str = localStorage.getItem(propertyName)
+//             let abc = str ? JSON.parse(str) : null
+//             const result = method.apply(this, abc ? [abc] : args);
+//             console.log(propertyName)
+//             localStorage.setItem(propertyName, JSON.stringify(result)) 
+//             return result;
+//         };
+//     };
+// }
+
+const defaultState = {gameType: 0, countColors: 5, fieldType: 0, startCellsType: 0, fieldSize: 0, botLevel: 0, seed: undefined}
+
+// class NewGameStore {
+//     @saveToLocalStorage()
+//     useNeGame(initialState = defaultState) {
+//         let [gameType, setGameType] = useState(initialState.gameType ? initialState.gameType : defaultState.gameType)
+//         let [fieldType, setFieldType] = useState(initialState.fieldType ? initialState.fieldType : defaultState.fieldType)
+//         let [countColors, setCountColors] = useState(initialState.countColors ? initialState.countColors : defaultState.countColors)
+//         let [startCellsType, setStartCellsType] = useState(initialState.startCellsType ? initialState.startCellsType : defaultState.startCellsType)
+//         let [fieldSize, setFieldSize] = useState(initialState.fieldSize ? initialState.fieldSize : defaultState.fieldSize)
+//         let [botLevel, setBotLevel] = useState(initialState.botLevel ? initialState.botLevel : defaultState.botLevel)
+//         let [seed, setSeed] = useState(initialState.seed ? initialState.seed : defaultState.seed)
+
+//         useEffect(() => {
+//             setStartCellsType(defaultState.startCellsType)
+//             setFieldSize(defaultState.fieldSize)
+//         }, [fieldType])
+
+//         return {
+//             gameType, setGameType, 
+//             fieldType, setFieldType, 
+//             countColors, setCountColors, 
+//             startCellsType, setStartCellsType,
+//             fieldSize, setFieldSize,
+//             botLevel, setBotLevel,
+//             seed, setSeed,
+//         } 
+//     }
+// }
+
+const useNewGame = (initialState = defaultState) => {
+    let [gameType, setGameType] = useState(initialState.gameType ? initialState.gameType : defaultState.gameType)
+    let [fieldType, setFieldType] = useState(initialState.fieldType ? initialState.fieldType : defaultState.fieldType)
+    let [countColors, setCountColors] = useState(initialState.countColors ? initialState.countColors : defaultState.countColors)
+    let [startCellsType, setStartCellsType] = useState(initialState.startCellsType ? initialState.startCellsType : defaultState.startCellsType)
+    let [fieldSize, setFieldSize] = useState(initialState.fieldSize ? initialState.fieldSize : defaultState.fieldSize)
+    let [botLevel, setBotLevel] = useState(initialState.botLevel ? initialState.botLevel : defaultState.botLevel)
+    let [seed, setSeed] = useState(initialState.seed ? initialState.seed : defaultState.seed)
+
+    useEffect(() => {
+        setStartCellsType(defaultState.startCellsType)
+        setFieldSize(defaultState.fieldSize)
+    }, [fieldType])
+
+    return {
+        gameType, setGameType, 
+        fieldType, setFieldType, 
+        countColors, setCountColors, 
+        startCellsType, setStartCellsType,
+        fieldSize, setFieldSize,
+        botLevel, setBotLevel,
+        seed, setSeed,
+    } 
 }
 
-const defaultState = {gameType: 0, countColors: 0, fieldType: 0, startCellsType: 0, fieldSize: 0, botLevel: 0, seed: undefined}
-
-class NewGameStore {
-    @saveToLocalStorage()
-    useNeGame(initialState = defaultState) {
-        let [gameType, setGameType] = useState(initialState.gameType ? initialState.gameType : defaultState.gameType)
-        let [fieldType, setFieldType] = useState(initialState.fieldType ? initialState.fieldType : defaultState.fieldType)
-        let [countColors, setCountColors] = useState(initialState.countColors ? initialState.countColors : defaultState.countColors)
-        let [startCellsType, setStartCellsType] = useState(initialState.startCellsType ? initialState.startCellsType : defaultState.startCellsType)
-        let [fieldSize, setFieldSize] = useState(initialState.fieldSize ? initialState.fieldSize : defaultState.fieldSize)
-        let [botLevel, setBotLevel] = useState(initialState.botLevel ? initialState.botLevel : defaultState.botLevel)
-        let [seed, setSeed] = useState(initialState.seed ? initialState.seed : defaultState.seed)
-
-        useEffect(() => {
-            setStartCellsType(defaultState.startCellsType)
-            setFieldSize(defaultState.fieldSize)
-        }, [fieldType])
-
-        return {
-            gameType, setGameType, 
-            fieldType, setFieldType, 
-            countColors, setCountColors, 
-            startCellsType, setStartCellsType,
-            fieldSize, setFieldSize,
-            botLevel, setBotLevel,
-            seed, setSeed,
-        } 
-    }
-}
-
-export default createContainer(new NewGameStore().useNeGame)
+export default createContainer(useNewGame, true)
--- src/Store/SettingsStore.ts
+++ src/Store/SettingsStore.ts
@@ -1,45 +1,63 @@
 import React, { useEffect, useState } from 'react'
 import { createContainer } from 'react-gstore'
 
-function saveToLocalStorage() {
-    return (target: Object, propertyName: string, descriptor: any) => {
-        const method = descriptor.value;
-        descriptor.value = function(...args : any[]) {
-            console.log(args)
-            let str = localStorage.getItem(propertyName)
-            let abc = str ? JSON.parse(str) : null
-            const result = method.apply(this, abc ? [abc] : args);
-            console.log(propertyName)
-            localStorage.setItem(propertyName, JSON.stringify(result)) 
-            return result;
-        };
-    };
+// function saveToLocalStorage() {
+//     return (target: Object, propertyName: string, descriptor: any) => {
+//         const method = descriptor.value;
+//         descriptor.value = function(...args : any[]) {
+//             console.log(args)
+//             let str = localStorage.getItem(propertyName)
+//             let abc = str ? JSON.parse(str) : null
+//             const result = method.apply(this, abc ? [abc] : args);
+//             console.log(propertyName)
+//             localStorage.setItem(propertyName, JSON.stringify(result)) 
+//             return result;  
+//         };
+//     };
+// }
+
+// class Store {
+//     @saveToLocalStorage()
+//     useSettings(initialState = {theme: 0, name: '', lang: 'en', devMode: false}) {
+//         let [theme, setTheme] = useState(initialState.theme)
+//         let [name, setName] = useState(initialState.name)
+//         let [devMode, setDevMode] = useState(initialState.devMode)
+//         let [lang, setLang] = useState(initialState.lang)
+//         let dark = () => setTheme(0)
+//         let light = () => setTheme(1)
+//         let anotherTheme = () => setTheme((theme + 1) % 2)
+
+//         return {
+//             theme, dark, 
+//             light, anotherTheme,
+//             name, setName,
+//             lang, setLang,
+//             devMode, setDevMode
+//         }
+//     }
+
+//     @saveToLocalStorage()
+//     call(initialState : any) {
+//         return this.useSettings(initialState)
+//     }
+// }
+
+const useSettings = (initialState = {theme: 0, name: '', lang: 'en', devMode: false}) => {
+    let [theme, setTheme] = useState(initialState.theme)
+    let [name, setName] = useState(initialState.name)
+    let [devMode, setDevMode] = useState(initialState.devMode)
+    let [lang, setLang] = useState(initialState.lang)
+    let dark = () => setTheme(0)
+    let light = () => setTheme(1)
+    let anotherTheme = () => setTheme((theme + 1) % 2)
+
+    return {
+        theme, dark, 
+        light, anotherTheme,
+        name, setName,
+        lang, setLang,
+        devMode, setDevMode
+    }
 }
 
-class Store {
-    @saveToLocalStorage()
-    useSettings(initialState = {theme: 0, name: '', lang: 'en', devMode: false}) {
-        let [theme, setTheme] = useState(initialState.theme)
-        let [name, setName] = useState(initialState.name)
-        let [devMode, setDevMode] = useState(initialState.devMode)
-        let [lang, setLang] = useState(initialState.lang)
-        let dark = () => setTheme(0)
-        let light = () => setTheme(1)
-        let anotherTheme = () => setTheme((theme + 1) % 2)
-
-        return {
-            theme, dark, 
-            light, anotherTheme,
-            name, setName,
-            lang, setLang,
-            devMode, setDevMode
-        }
-    }
-
-    @saveToLocalStorage()
-    call(initialState : any) {
-        return this.useSettings(initialState)
-    }
-}
-
-export default createContainer(new Store().useSettings)
+export default createContainer(useSettings, true)
